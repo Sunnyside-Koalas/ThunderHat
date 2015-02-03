f727c87e385d6ba5fbe838aa0084d5e4cfc365cd
cfmeyers <cfmeyers@gmail.com>
2015-02-03 07:17:55 -0500

    add top 20 from API

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,18 @@
 class StaticPagesController < ApplicationController
+  require 'soundcloud'
   def top_twenty
-    sc = SoundCloudWrapper.new
+    # sc = SoundCloudWrapper.new
+    client_id = ENV["SOUNDCLOUD_CLIENT_ID"]
+    client = SoundCloud.new(:client_id => client_id)
+    tracks = client.get('/tracks', :limit => 20, :order => 'hotness')
+    @tracks = []
+    tracks.each do |track|
+      t = Track.create( title: track["title"], 
+                   permalink_url:track["permalink_url"], 
+                   description:track["description"] , 
+                   avatar_url:track["user"]["avatar_url"] )
+      @tracks << t
+    end
 
   end
 end
